8e6848148072dfb8f0b3e80797aba410d3b3fb4e
KahoMoon <kahomoon1@gmail.com>
2023-05-14 14:20:27 -0700

    Made changes to constructor and setters. Working on comparison functions.

--- src/MangaEntry.java
+++ src/MangaEntry.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Objects;
@@ -23,17 +24,32 @@ public class MangaEntry {
     Status status;
     Date startPublication;
     Date endPublication;
-    HashSet<Genre> genre;
+    HashSet<Genre> genres;
+
+    public MangaEntry(String title, HashSet<String> altTitles, int numOfChapters, int numOfVolumes, Status status, int startMonth, int startDay, int startYear, int endMonth, int endDay, int endYear, HashSet<Genre> genres) {
+        this.setTitle(title);
+        this.setAltTitles(altTitles);
+        this.setNumOfChapters(numOfChapters);
+        this.setNumOfVolumes(numOfVolumes);
+        this.setStatus(status);
+        this.setStartPublication(startMonth, startDay, startYear);
+        this.setEndPublication(endMonth, endDay, endYear);
+        this.setGenres(genres);
+    }
 
     public MangaEntry(String title, HashSet<String> altTitles, int numOfChapters, int numOfVolumes, Status status, Date startPublication, Date endPublication, HashSet<Genre> genres) {
-        this.title = title;
-        this.altTitles = altTitles;
-        this.numOfChapters = numOfChapters;
-        this.numOfVolumes = numOfVolumes;
-        this.status = status;
-        this.startPublication = startPublication;
-        this.endPublication = endPublication;
-        this.genre = genres;
+        this.setTitle(title);
+        this.setAltTitles(altTitles);
+        this.setNumOfChapters(numOfChapters);
+        this.setNumOfVolumes(numOfVolumes);
+        this.setStatus(status);
+        this.setStartPublication(startPublication);
+        this.setEndPublication(endPublication);
+        this.setGenres(genres);
+    }
+
+    public MangaEntry() {
+
     }
 
     public String getTitle() {
@@ -49,7 +65,13 @@ public class MangaEntry {
     }
 
     public void setAltTitles(HashSet<String> altTitles) {
-        this.altTitles = altTitles;
+        System.out.println(altTitles);
+        HashSet<String> altTitlesTemp = new HashSet<>();
+        for (String i : altTitles) {
+            altTitlesTemp.add(i.toLowerCase());
+        }
+        this.altTitles = altTitlesTemp;
+        System.out.println(altTitlesTemp);
     }
 
     public int getNumOfChapters() {
@@ -80,6 +102,14 @@ public class MangaEntry {
         return startPublication;
     }
 
+    public void setStartPublication(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        this.startPublication = cal.getTime();
+    }
+
     public void setStartPublication(Date startPublication) {
         this.startPublication = startPublication;
     }
@@ -88,16 +118,24 @@ public class MangaEntry {
         return endPublication;
     }
 
+    public void setEndPublication(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        this.endPublication = cal.getTime();
+    }
+
     public void setEndPublication(Date endPublication) {
         this.endPublication = endPublication;
     }
 
-    public HashSet<Genre> getGenre() {
-        return genre;
+    public HashSet<Genre> getGenres() {
+        return genres;
     }
 
-    public void setGenre(HashSet<Genre> genre) {
-        this.genre = genre;
+    public void setGenres(HashSet<Genre> genres) {
+        this.genres = genres;
     }
 
     private int equalsHelper(Object obj) {
@@ -113,10 +151,7 @@ public class MangaEntry {
 
         final MangaEntry other = (MangaEntry) obj;
         boolean titleMatch = this.title.equals(other.title);
-        boolean
-        if () {
 
-        }
 
         return -99;
     }
@@ -125,16 +160,38 @@ public class MangaEntry {
     * Returns a float representing the percentage which the titles match
     * @param entry the entry being compared against
     */
-    private float compareTitle(MangaEntry entry) {
+    float compareTitles(MangaEntry entry) {
 
         if (Objects.equals(this.title, entry.title)) {
             return 100;
         } else if (this.altTitles.contains(entry.title) || entry.altTitles.contains(this.title)) {
             return 100;
+        } else {
+            int numOfMatches = 0;
+            int numOfComparison = 1;
+
+            if (this.altTitles.size() <= entry.altTitles.size()) {
+                for (String i : this.altTitles) {
+                    if (entry.altTitles.contains(i)) {
+                        numOfMatches++;
+                    }
+                    numOfComparison++;
+                }
+            } else {
+                for (String i : entry.altTitles) {
+                    if (this.altTitles.contains(i)) {
+                        numOfMatches++;
+                    }
+                    numOfComparison++;
+                }
+            }
+            return (float) numOfMatches / numOfComparison;
         }
 
-        return 0;
+    }
 
+    float compareNumOfChapters(MangaEntry entry) {
+        return (float) Math.min(this.numOfChapters, entry.numOfChapters) / Math.max(this.numOfChapters, entry.numOfChapters);
     }
 
 }
